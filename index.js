ed191e3efb7990f1780315fe4ede30de1417663c
Jamie Humphries <jamie.humphries@softwire.com>
2024-09-06 14:16:32 +0100

    Convert sprites for emoji using imagemagick

---
+++ index.js
@@ -0,0 +1,40 @@
+import { readFileSync } from "fs";
+import { globSync } from "glob";
+import { convert } from "imagemagick";
+import { parse } from "path";
+
+const sprites = globSync("sprites/sprites/pokemon/*.png").filter((path) =>
+  path.match(/\/[1-9]\d{0,3}\.png$/g)
+);
+
+for (const sprite of sprites) {
+  const number = parse(sprite).name;
+  const paddedNumber = getPaddedNumber(number);
+  const name = getName(number);
+  const output = `emoji/pokemon-${paddedNumber}-${name}.png`;
+  convert([sprite, "-trim", "+repage", output], convertHandler);
+}
+
+function getPaddedNumber(number) {
+  let paddedNumber = number;
+  while (paddedNumber.length < 3) {
+    paddedNumber = "0" + paddedNumber;
+  }
+  return paddedNumber;
+}
+
+function getName(number) {
+  const apiFilepath = `api-data/data/api/v2/pokemon/${number}/index.json`;
+  const content = readFileSync(apiFilepath);
+  const json = JSON.parse(content);
+  return json.name;
+}
+
+function convertHandler(err, stdout) {
+  if (err) {
+    throw err;
+  }
+  if (stdout) {
+    console.log(stdout);
+  }
+}
